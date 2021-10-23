218d5de98928ea838b6352c3d6dbcc7d48ffaedf
Ashique <ashique12009@gmail.com>
2021-10-23 20:02:58 +0600

    nav modified for logged in and guest user

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,9 +3,13 @@
     <div class="container">
       <router-link to="/" class="navbar-brand">Home</router-link>
       <div class="collapse navbar-collapse">
-        <ul class="navbar-nav ml-auto">
+        <ul class="navbar-nav ml-auto" v-if="!user">
           <li class="nav-item"><router-link to="Login" class="nav-link">Login</router-link></li>
           <li class="nav-item"><router-link to="Register" class="nav-link">Signup</router-link></li>
+        </ul>
+
+        <ul class="navbar-nav ml-auto" v-if="user">
+          <li class="nav-item"><router-link to="Login" class="nav-link">Logout</router-link></li>
         </ul>
       </div>
     </div>
@@ -13,8 +17,22 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'Nav'
+  name: 'Nav',
+  data() {
+    return {
+      user: ''
+    }
+  },
+  async created() {
+    const response = await axios.get('user');
+
+    if (response.data.id != '') {
+      this.user = response.data;
+    }
+  }
 }
 </script>
 
